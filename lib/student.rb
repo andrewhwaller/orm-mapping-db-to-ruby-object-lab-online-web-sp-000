77f253c77f9bc6dab6f1282c788a9ceb6846e713
Andrew Waller <andrewhwaller@gmail.com>
2019-04-15 00:19:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -110,4 +110,16 @@ class Student
     end.first
   end
 
+  def self.first_X_students_in_grade_10(limit)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, limit).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
